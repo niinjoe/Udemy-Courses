57cc121619fb2a6b8e152391d56248d7474c18ac
Joe <80960873+niinjoe@users.noreply.github.com>
2023-03-17 15:35:16 -0400

    updates on new day 25

--- 100 Days of Code/Day 25/main.py
+++ 100 Days of Code/Day 25/main.py
@@ -2,14 +2,30 @@
 #     data_list = data.readlines()
 #     print(data_list)
 
-import csv
+# import csv
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        temperatures.append(row[1])
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         temperatures.append(row[1])
     
-    temperatures.remove(temperatures[0])
-    new_temp_ls = list(map(int, temperatures))
-    print(new_temp_ls)
+#     temperatures.remove(temperatures[0])
+#     new_temp_ls = list(map(int, temperatures))
+#     print(new_temp_ls)
+
+import pandas as pd
+
+data = pd.read_csv("weather_data.csv")
+
+# print(data["temp"])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+temp_list = data["temp"].tolist()
+
+# print(temp_list)
+
+avg = temp_list.sum()/temp_list.len()
+print(avg)
