8f6dc34229657429d6ab3f00f1c2459c688c0b27
Joe <80960873+niinjoe@users.noreply.github.com>
2023-03-16 20:24:47 -0400

    trying instructors code

--- 100 Days of Code/Day 24/223_challenge/Mail Merge Project Start/main.py
+++ 100 Days of Code/Day 24/223_challenge/Mail Merge Project Start/main.py
@@ -18,3 +18,16 @@ with open("./Input/Letters/starting_letter.txt", "r") as main:
                 writer.write(f"Dear {ind},\n\n")
                 writer.write(message.strip("Dear [name],\n"))
 
+####### Instructor's Code:
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", "w") as completed_letter:
+            completed_letter.write(new_letter)
