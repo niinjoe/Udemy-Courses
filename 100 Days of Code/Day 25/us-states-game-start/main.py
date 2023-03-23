e65e48af31807baa15453cdf52b954b1ec49100e
Joe <80960873+niinjoe@users.noreply.github.com>
2023-03-23 10:42:19 -0400

    game completed

--- 100 Days of Code/Day 25/us-states-game-start/main.py
+++ 100 Days of Code/Day 25/us-states-game-start/main.py
@@ -10,34 +10,56 @@ screen.addshape(image)
 turtle.shape(image)
 
 game_on = True
-results = []
-count = len(results)
+results = df["state"].tolist()
 
 while game_on:
+    states_list = df["state"].tolist()
+    count = len(results)
+
+    if count == 50:
+        answer_state = screen.textinput(
+            title="Game completed.",
+            prompt="Press Q to quit or S to start again.",
+        ).title()
+        if answer_state == "Q":
+            break
+        elif answer_state == "S":
+            results = []
+            count = 0
+        else:
+            answer_state = screen.textinput(
+                title=f"{count}/50 States Correct",
+                prompt="Invalid option. Press Q to quit or S to start again.",
+            ).title()
+
     answer_state = screen.textinput(
         title=f"{count}/50 States Correct", prompt="Give me a state name."
     ).title()
 
-    # if answer_state is not None:
-    #     answer_state = answer_state.title()
-    #     print(answer_state)
-    # else:
-    #     print("User clicked Cancel.")
-
-    result_row = df.loc[df["state"] == answer_state]
-    # results.append(answer_state)
-
-    states_list = df["states"].tolist()
     if answer_state in states_list:
+        result_row = df.loc[df["state"] == answer_state]
+        if not result_row.empty:
+            x_cor = result_row["x"].iloc[0]
+            y_cor = result_row["y"].iloc[0]
+
+        my_turtle.penup()
+        my_turtle.goto(x_cor, y_cor)
+        my_turtle.write(answer_state)
         results.append(answer_state)
 
-    if not result_row.empty:
-        x_cor = result_row["x"].iloc[0]
-        y_cor = result_row["y"].iloc[0]
+    while answer_state not in states_list:
+        answer_state = screen.textinput(
+            title=f"{count}/50 States Correct",
+            prompt="Invalid answer. Please try again.",
+        ).title()
+        if answer_state in states_list:
+            result_row = df.loc[df["state"] == answer_state]
+            results.append(answer_state)
 
-    my_turtle.penup()
-    my_turtle.goto(x_cor, y_cor)
-    my_turtle.write(answer_state)
+            if not result_row.empty:
+                x_cor = result_row["x"].iloc[0]
+                y_cor = result_row["y"].iloc[0]
 
-
-print(results)
+            my_turtle.penup()
+            my_turtle.goto(x_cor, y_cor)
+            my_turtle.write(answer_state)
