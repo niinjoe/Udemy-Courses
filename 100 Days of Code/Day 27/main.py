8ac0911e95d19bc7abe2f49b79374b8f3d47045a
Joe <80960873+niinjoe@users.noreply.github.com>
2023-03-29 21:31:36 -0400

    button challenge

--- 100 Days of Code/Day 27/main.py
+++ 100 Days of Code/Day 27/main.py
@@ -1,10 +1,30 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(500, 300)
-window.mainloop()
 
 # Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+# Button
+def button_clicked():
+    print("I got clicked.")
+
+if button_clicked:
+    my_label.config(text="I got clicked.")
+
+
+button =  Button(text="Click Me", command=button_clicked)
+button.pack()
+
+
+
+
+
+
+window.mainloop()
