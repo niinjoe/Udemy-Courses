0b21b30ebd496e1cfd6c7573408cad578c6fbbb9
Joe <80960873+niinjoe@users.noreply.github.com>
2023-03-29 20:33:47 -0400

    following up on course

--- 100 Days of Code/Day 27/playground.py
+++ 100 Days of Code/Day 27/playground.py
@@ -3,9 +3,24 @@
 
 # print(add([88, 43, 123]))
 
+
 def add(*args):
     for n in args:
         n += n
     return n
 
-print(add(1,2,3,90,132))
+
+# print(add(1,2,3,90,132))
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(5, add=3, multiply=5)
