8fe3561444964224294694e54c564e11dc9c584c
Joe <80960873+niinjoe@users.noreply.github.com>
2023-03-30 19:43:20 -0400

    added instructors code and a conceptual project

---
+++ 100 Days of Code/Day 27/installer.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+import os
+import subprocess
+
+path = "C:/Programs"
+
+window = tk.Tk()
+window.title("Software Installation")
+title = tk.Label(text="Select the software you want to install:")
+title.pack()
+
+app1 = tk.Checkbutton(text="App1")
+app1.pack()
+
+app2 = tk.Checkbutton(text="App2")
+app2.pack()
+
+app3 = tk.Checkbutton(text="App3")
+app3.pack()
+
+def install_apps():
+    if app1.get() == 1:
+        subprocess.call(os.path.join(path, "app1.exe"))
+    if app2.get() == 1:
+        subprocess.call(os.path.join(path, "app2.exe"))
+    if app3.get() == 1:
+        subprocess.call(os.path.join(path, "app3.exe"))
+
+install_button = tk.Button(text="Install", command=install_apps)
+install_button.pack()
+
+quit_button = tk.Button(text="Quit", command=window.quit)
+quit_button.pack()
+
+window.mainloop()
--- 100 Days of Code/Day 27/main.py
+++ 100 Days of Code/Day 27/main.py
@@ -14,17 +14,27 @@ my_label.config(text="New Text")
 # Button
 def button_clicked():
     print("I got clicked.")
+    new_text = input.get()
+    my_label.config(text=new_text)
 
-if button_clicked:
-    my_label.config(text="I got clicked.")
-
-
-button =  Button(text="Click Me", command=button_clicked)
+button = Button(text="Click Me", command=button_clicked)
 button.pack()
 
-
-
+input = Entry(width=10)
+input.pack()
+input.get()
 
 
 
 window.mainloop()
+
+# Components of a GUI window
+# Label = text displayed as information
+# Button = button that will activate on a click, and can collect information or activate a function
+# Entry = text box to record user input
+# Text = multi-line text box to record user input
+# Spinbox = display box with options to select one using arrow buttons
+# Scale = draggable bar to select an option
+# Checkbutton = small checkbox to select an option
+# Radiobutton = similar to checkboxes, but rounded
+# Listbox = text box with a list of strings
