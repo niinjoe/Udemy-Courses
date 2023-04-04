226d1d824e492380ac18904d8600d8679ffd9e63
Joe <80960873+niinjoe@users.noreply.github.com>
2023-04-04 13:44:06 -0400

    day 28 project finished

--- 100 Days of Code/Day 28/pomodoro-start/main.py
+++ 100 Days of Code/Day 28/pomodoro-start/main.py
@@ -2,40 +2,59 @@ from tkinter import *
 import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+check_list = []
+timer = None
+
 # ---------------------------- TIMER RESET ------------------------------- #
 
+
+def reset_press():
+    window.after_cancel(timer)
+    timer_title.config(text="Timer", fg=GREEN)
+    checkmarks.config(text="")
+    canvas.itemconfig(timer_text, text=f"00:00")
+    global reps
+    reps = 0
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+
 
 def timer_start():
     # count_down(5*60)
     global reps
-    work_sec = WORK_MIN *60
-    short_break_sec = SHORT_BREAK_MIN *60
-    long_break_sec = LONG_BREAK_MIN *60
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
-    #If its the 1st,3rd,5th,7th rep:
-    if reps == 0:
-      count_down(work_sec)
-      reps = 1
-    #If its the 8th rep:
-    # count_down(long_break_sec)
-    #If its 2nd,4th,6th rep:
-    # count_down(short_break_sec)
+    if reps % 8 == 0:
+        timer_title.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        timer_title.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        timer_title.config(text="Working", fg=GREEN)
+        count_down(work_sec)
+        check_list.append("✓")
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
 def count_down(count):
-    count_min = math.floor(count/60)
+    count_min = math.floor(count / 60)
     if count_min < 10:
         count_min = f"0{count_min}"
     count_sec = count % 60
@@ -44,7 +63,11 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    if count == 0:
+        checkmarks.config(text=[c for c in check_list])
+        timer_start()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -63,17 +86,17 @@ timer_text = canvas.create_text(
 canvas.grid(column=1, row=1)
 
 # Labels
-timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
-timer.grid(column=1, row=0)
+timer_title = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
+timer_title.grid(column=1, row=0)
 
-checkmarks = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
+checkmarks = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
 checkmarks.grid(column=1, row=3)
 
 # Buttons
 start = Button(text="Start", command=timer_start)
 start.grid(column=0, row=2)
 
-reset = Button(text="Reset")
+reset = Button(text="Reset", command=reset_press)
 reset.grid(column=2, row=2)
 
 
