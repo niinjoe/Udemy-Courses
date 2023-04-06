bc299c6a9119304560b27cf7f4d1ce8fdf6c51b3
Joe <80960873+niinjoe@users.noreply.github.com>
2023-04-05 21:03:59 -0400

    loose_end

--- 100 Days of Code/Day 27/main.py
+++ 100 Days of Code/Day 27/main.py
@@ -1,28 +1,27 @@
 from tkinter import *
 
+def button_clicked():
+    print("I got clicked.")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(500, 300)
 
 # Label
 my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.config(text="New Text")
 my_label.pack()
 
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
-
 # Button
-def button_clicked():
-    print("I got clicked.")
-    new_text = input.get()
-    my_label.config(text=new_text)
-
 button = Button(text="Click Me", command=button_clicked)
 button.pack()
 
+# Entry
 input = Entry(width=10)
+print(input.get())
 input.pack()
-input.get()
 
 
 
