f835be3f8a1ae66255f979dae777f492e33ecf71
Joe <80960873+niinjoe@users.noreply.github.com>
2023-04-21 15:59:09 -0400

    challenge start

--- 100 Days of Code/Day 32/birthday-wisher-extrahard-start/main.py
+++ 100 Days of Code/Day 32/birthday-wisher-extrahard-start/main.py
@@ -2,7 +2,7 @@
 
 # 1. Update the birthdays.csv - DONE
 
-# 2. Check if today matches a birthday in the birthdays.csv
+# 2. Check if today matches a birthday in the birthdays.csv - DONE-ish
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
@@ -13,17 +13,20 @@
 ############# Step 1: Read the csv to comply with point #2
 
 import pandas as pd
-df = pd.read_csv("birthdays.csv")
-birthdates = df.to_dict(orient="records")
-print(birthdates)
-
-
 import datetime as dt
+
 now = dt.datetime.now()
-# year = now.year
 month = now.month
-day_of_week = now.weekday()
-# print(now)
+today = now.day
+# print(f"Month: {month}, Day: {today}")
+
+
+df = pd.read_csv("birthdays.csv")
+bday = df[(df["month"] == month) & (df["day"] == today)]
+if not bday.empty:
+    print("True")
+else:
+    print("False")
 
 
 ############# Step 2: Follow point #3
@@ -31,9 +34,13 @@ day_of_week = now.weekday()
 from random import choice
 
 letter = choice(["letter_1.txt", "letter_2.txt", "letter_3.txt"])
-with open(f"./letter_templates/{letter}") as file:
-    letter = file.read()
-    # letter = letter.replace("[NAME]", f"{df.name}")
 
+old_value = "[NAME]"
+new_value = bday["name"]
+print(new_value)
+
+# with open(f"./letter_templates/{letter}", "w") as file:
+#     letter = file.read()
+#     letter = letter.replace("[NAME]", f"{df.name}")
     # print(letter)
     
