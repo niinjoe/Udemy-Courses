fd09783a2877ccca91fb5a2a67f656ea60d02167
Joe <80960873+niinjoe@users.noreply.github.com>
2023-04-24 11:58:25 -0400

    day 32 challenge completed

--- 100 Days of Code/Day 32/birthday-wisher-extrahard-start/main.py
+++ 100 Days of Code/Day 32/birthday-wisher-extrahard-start/main.py
@@ -2,9 +2,9 @@
 
 # 1. Update the birthdays.csv - DONE
 
-# 2. Check if today matches a birthday in the birthdays.csv - DONE-ish
+# 2. Check if today matches a birthday in the birthdays.csv - DONE
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv - DONE
 
 # 4. Send the letter generated in step 3 to that person's email address.
 
@@ -18,16 +18,13 @@ import datetime as dt
 now = dt.datetime.now()
 month = now.month
 today = now.day
-# print(f"Month: {month}, Day: {today}")
-
 
 df = pd.read_csv("birthdays.csv")
 bday = df[(df["month"] == month) & (df["day"] == today)]
 if not bday.empty:
-    print("True")
+    bday_person = bday.iloc[0]["name"]
 else:
-    print("False")
-
+    print("No birthdays today.")
 
 ############# Step 2: Follow point #3
 
@@ -35,12 +32,26 @@ from random import choice
 
 letter = choice(["letter_1.txt", "letter_2.txt", "letter_3.txt"])
 
-old_value = "[NAME]"
-new_value = bday["name"]
-print(new_value)
-
-# with open(f"./letter_templates/{letter}", "w") as file:
-#     letter = file.read()
-#     letter = letter.replace("[NAME]", f"{df.name}")
+with open(f"./letter_templates/{letter}") as file:
+    letter = file.read()
+    letter = letter.replace("[NAME]", bday_person)
     # print(letter)
     
+############# Final Step: Send message via email
+
+import smtplib
+
+# my_email = "ed.serrano.test@gmail.com"  # This is the sender email
+my_email = "daftsapien@gmail.com"  # This is the sender email
+# password = "maerzcuqgyrrjedd"  # This is the app password, not the account password (for google)
+password = "vmigvmepcdghnnqj"  # This is the app password, not the account password (for google)
+test_email = "joe_serrano_test@yahoo.com"  # This is the receivers email
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=test_email,
+        msg=f"Subject:Happy Birthday!\n\n{letter}",
+        )
